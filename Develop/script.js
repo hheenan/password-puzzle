40e395e67e8150e51fc4885fcb73c861ce2c9359
Hannah <heenan93@gmail.com>
2022-04-23 10:12:17 -0700

    add function and array

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
+// constants for the characters
+const generate = document.getElementById('generate')
+generate.addEventListener('click', writePassword)
 
+console.log(generate)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -16,15 +20,34 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var numarr = ["0","1","2","3","4","5","6","7","8","9"]
+
+
 //Upon click of generate password 
 
 function generatePassword () {
-  var passwordLength =prompt("Enter the number of characters in new password please. The password must be greater than 12 but less than 128");
+  var passwordLength = parseInt(prompt("Enter the number of characters in new password please. The password must be greater than 12 but less than 128"));
   var numbers = confirm ("Do you want NUMBERS in your password?");
   var lowerCases= confirm("Do you want LOWERCASES in your password?");
   var upperCasses = confirm("Do you want UPPERCASES in your password?");
   var special = confirm("Do you want SPECIAL CHARACTERS in your password?");
+ 
+  var possiblecharacters = []
+  if (numbers) {
+    possiblecharacters = possiblecharacters.concat(numarr)
+  }
+  for (let index = 0; index < passwordLength; index++) {
+    var character = getRandom(possiblecharacters)
+    push (character)
+  }
 }
+// array for variables above
+function getRandom (arr) {
+  var randomIndex = Math.floor(Math.random()* arr.length )
+  var randomElement = arr[randomIndex]
+  return randomElement
+}
+
 // minimum count for variables above
 var minimumCount = 0;
 // empty string for character variables
@@ -32,4 +55,6 @@ var minimumNumbers = "";
 var minimumLowerCases = "";
 var minimumUpperCases = "";
 var minimumSpecialCharacters = "";
+
+
 
